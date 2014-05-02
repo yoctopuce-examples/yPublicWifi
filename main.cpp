5dd55d4d472aa06aa524271ed11ae9ed7570f203
Yoctopuce <yocto@yocto-K53E.(none)>
2014-05-02 22:26:08 +0200

    improve ui

--- main.cpp
+++ main.cpp
@@ -9,27 +9,65 @@
 
 using namespace std;
 
-#define WELCOME_MSG "Press for WiFi"
+#define HOTSPOT_NAME "yHotSpot"
 #define PASSWORD_EXPIRATION (3600) //password expire after 1 hour
-#define PASSWORD_LENGHT (10) 
+#define PASSWORD_LENGTH     (10) 
 #define WIFI_CONFIG_FILE "/etc/config/wireless"
 #define WIFI_CONFIG_TMP_FILE "/tmp/wireless.tmp"
 #define WIFI_CONFIG_OLD_FILE "/etc/config/wireless.old"
+#define PRESS_BUTTON_SEQ  "press.seq"
 
 static const char allowedchar[] =
 "23456789."
-"ABCDEFGHIJKMNPQRSTUVWXYZ"
+"ABCDEFGHJKMNPQRSTUVWXYZ"
 "abcdefghjkmnpqrstuvwxyz";
 
 YDisplay    *Display = NULL;
 YAnButton   *Button = NULL;
-int         IsWifiUp=0;
+int         IsWifiUp = 0;
+char        SecretKey[PASSWORD_LENGTH+1];
 time_t      PasswordExpiration=0;
 
-static void showMessage(const char *msg)
-{
+typedef enum {
+    WELCOME_MSG, STARTING_MSG, HOTSPOT_MSG, COLDSPOT_MSG
+} MSG_TYPE;
 
-    cout << msg << endl;
+static void animateArrow(void)
+{
+    YDisplayLayer  *l1 = Display->get_displayLayer(1);
+    int             hw = 4;
+
+    l1->hide();
+    l1->clear();
+    l1->moveTo(64-hw,64-6*hw);
+    l1->lineTo(64+hw,64-6*hw);
+    l1->lineTo(64+hw,64-4*hw);
+    l1->lineTo(64+2*hw,64-4*hw);
+    l1->lineTo(64,64-hw);
+    l1->lineTo(64-2*hw,64-4*hw);
+    l1->lineTo(64-hw,64-4*hw);
+    l1->lineTo(64-hw,64-6*hw);
+    l1->setLayerPosition(0, -hw, 0);
+    l1->unhide();
+    l1->setLayerPosition(0, hw-1, 700);
+}
+
+static void showMessage(MSG_TYPE msg)
+{
+    switch(msg) {
+        case WELCOME_MSG:
+            cout << "INFO: Welcome message" << endl;
+            break;
+        case STARTING_MSG:
+            cout << "INFO: Starting WiFi" << endl;
+            break;
+        case HOTSPOT_MSG:
+            cout << "INFO: HotSpot started, key=" << SecretKey << endl;
+            break;
+        case COLDSPOT_MSG:
+            cout << "INFO: Canoot start WiFi" << endl;
+            break;
+    }
 
     if (Display==NULL || !Display->isOnline()) {
         cerr << "No Yocto-Display connected (check USB cable)" << endl;
@@ -41,13 +79,28 @@ static void showMessage(const char *msg)
     int w = Display->get_displayWidth();
     int h = Display->get_displayHeight();
 
-    // reteive the first layer
+    // display text on first layer
     YDisplayLayer *l0 = Display->get_displayLayer(0);
-    l0->clear();
-
-    // display a text in the middle of the screen
     l0->selectFont("Medium.yfm");
-    l0->drawText(w / 2, h / 2, YDisplayLayer::ALIGN_CENTER, msg);
+    switch(msg) {
+        case WELCOME_MSG:
+            l0->drawText(w / 2, h * 3/8, YDisplayLayer::ALIGN_CENTER, "Press for WiFi");
+            Display->playSequence(PRESS_BUTTON_SEQ);
+            break;
+        case STARTING_MSG:
+            l0->drawText(w / 2, h / 2, YDisplayLayer::ALIGN_CENTER, "Starting WiFi...");
+            break;
+        case HOTSPOT_MSG:
+            l0->drawText(w / 2, h * 3/8 - 2, YDisplayLayer::ALIGN_CENTER, HOTSPOT_NAME);
+            l0->drawText(w / 2, h * 7/8 - 3, YDisplayLayer::ALIGN_CENTER, SecretKey);
+            l0->selectFont("Small.yfm");
+            l0->drawText(w / 2, h * 1/8 - 2, YDisplayLayer::ALIGN_CENTER, "Hotspot name:");
+            l0->drawText(w / 2, h * 5/8 - 2, YDisplayLayer::ALIGN_CENTER, "Secret key:");
+            break;
+        case COLDSPOT_MSG:
+            l0->drawText(w / 2, h / 2, YDisplayLayer::ALIGN_CENTER, "Cannot start WiFi");
+            break;
+    }
 }
 
 static int startWiFi(const char *password)
@@ -85,8 +138,7 @@ static int startWiFi(const char *password)
         system("mv " WIFI_CONFIG_FILE " "WIFI_CONFIG_OLD_FILE);
         system("mv " WIFI_CONFIG_TMP_FILE " "WIFI_CONFIG_FILE);
         //reload config
-        system("wifi down");
-        system("wifi up");
+        system("wifi down ; wifi up &");
         IsWifiUp = 1;
     }
 
@@ -96,17 +148,17 @@ static int startWiFi(const char *password)
 static void stopWiFi(void)
 {
     if(IsWifiUp) {
-        system("wifi down");
+        system("wifi down &");
         showMessage(WELCOME_MSG);
         IsWifiUp=0;
     }
 }
 
-static void generatePasswd(char * buffer,int len)
+static void generatePasswd(char * buffer)
 {
     int i;
     int modulo = sizeof(allowedchar) - 1;
-    for (i=0; i< len-1 ; i++) {
+    for (i=0; i< PASSWORD_LENGTH; i++) {
         int idx = rand() % modulo;
         *buffer++ = allowedchar[idx];
     }
@@ -120,14 +172,13 @@ static void anButtonValueChangeCallBack(YAnButton *bt, const string& value)
     }
 
     if (!IsWifiUp) {
-        showMessage("starting WiFi...");
-        char newpass[PASSWORD_LENGHT+1];
-        generatePasswd(newpass,PASSWORD_LENGHT + 1);
-        if (startWiFi(newpass) < 0) {
-            showMessage("unable to start WiFi");
+        showMessage(STARTING_MSG);
+        generatePasswd(SecretKey);
+        if (startWiFi(SecretKey) < 0) {
+            showMessage(COLDSPOT_MSG);
             return;
         }
-        showMessage(newpass);
+        showMessage(HOTSPOT_MSG);
     }
 
     PasswordExpiration = time(NULL) + PASSWORD_EXPIRATION;
@@ -182,6 +233,7 @@ int main(int argc, const char * argv[])
             usage();
         }
     }
+    srand(time(NULL));
 
     stopWiFi();
     YAPI::RegisterDeviceArrivalCallback(deviceArrival);
@@ -195,10 +247,14 @@ int main(int argc, const char * argv[])
 
     while (true) {
         YAPI::UpdateDeviceList(errmsg); // traps plug/unplug events
-        YAPI::Sleep(1000, errmsg);   // traps others events
-        if (time(NULL) > PasswordExpiration ) {
-            stopWiFi();
+        if (IsWifiUp) {
+            if(time(NULL) > PasswordExpiration) {
+                stopWiFi();
+            } 
+        } else {
+            animateArrow();
         }
+        YAPI::Sleep(1000, errmsg);   // traps others events
     } 
     return 0;
 }
